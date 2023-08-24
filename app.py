afce05f39ca2525dce1a6a88dd74a3b656534250
Maryane Castro <122397850+maryane-castro@users.noreply.github.com>
2023-08-24 12:12:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#http://localhost:8501/
+
 import streamlit as st
 import cv2
 import numpy as np
@@ -8,7 +10,7 @@ from paddleocr import PaddleOCR
 ocr = PaddleOCR()
 
 # Função para desenhar caixas na imagem
-def draw_boxes_on_image(image, results):
+def draw_boxes_on_image_pdi(image, results):
     img_with_boxes = image.copy()
     for result in results[0]:
         text = result[1][0]
@@ -23,10 +25,42 @@ def draw_boxes_on_image(image, results):
         #cv2.putText(img_with_boxes, text, points[0], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     return img_with_boxes
 
-# Página 1
+
+
+
+def draw_boxes_on_image_nlp(image, results):
+    img_with_boxes = image.copy()
+    for result in results[0]:
+        text = result[1][0]
+        points = result[0]
+        
+        points = [(int(p[0]), int(p[1])) for p in points]
+
+        #for i in range(4):
+        #    cv2.line(img_with_boxes, points[i], points[(i+1) % 4], (0, 255, 0), 2)
+        
+
+        cv2.putText(img_with_boxes, text, points[0], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    return img_with_boxes
+
+
 def pag1():
-    st.title("Página 1")
-    st.write("pag1")
+    st.title("PDI")
+    uploaded_image = st.file_uploader("Escolha uma imagem...", type=["jpg", "png", "jpeg"])
+
+    if uploaded_image is not None:
+        with st.spinner('Detectando bounding box...'):
+            image = Image.open(uploaded_image)
+            image = np.array(image)  # converte a imagem PIL para uma matriz NumPy
+            
+            results = ocr.ocr(image)
+
+            image_with_boxes = draw_boxes_on_image_pdi(image, results)
+
+            st.image(image_with_boxes, caption='Imagem com Caixas Delimitadoras', use_column_width=True)
+
+def pag2():
+    st.title("NLP")
     uploaded_image = st.file_uploader("Escolha uma imagem...", type=["jpg", "png", "jpeg"])
 
     if uploaded_image is not None:
@@ -36,13 +70,9 @@ def pag1():
             
             results = ocr.ocr(image)
 
-            image_with_boxes = draw_boxes_on_image(image, results)
+            image_with_boxes = draw_boxes_on_image_nlp(image, results)
 
             st.image(image_with_boxes, caption='Imagem com Caixas Delimitadoras', use_column_width=True)
-
-def pag2():
-    st.title("NLP")
-    st.write("pag2")
 
 
 
